8bb3cde13a45ab3736f3068f3c12a6a4b502047b
sohanemon <m.sohan.rse@gmail.com>
2021-11-19 08:29:16 +0600

    completed without local storage

--- js/script.js
+++ js/script.js
@@ -1,2 +1,48 @@
-// document.querySelector("sendBtn").onclick = console.log("dkfjdf");
-document.querySelector("clearBtn").innerHTML = Cdfkjdf;
+// default ui element
+let form = document.querySelector("#task_form");
+let taskList = document.querySelector("#tasks");
+let clearBtn = document.querySelector("#clear_task_btn");
+let filter = document.querySelector("#task_filter");
+let taskInput = document.querySelector("#new_task");
+
+//define event listener
+form.addEventListener("submit", addTask);
+taskList.addEventListener("click", removeTask);
+clearBtn.addEventListener("click", removeAllTask);
+filter.addEventListener("keyup", filterTask);
+//functions
+function addTask(e) {
+  if (taskInput.value === "") alert("Eneter task");
+  else {
+    let li = document.createElement("li");
+    li.setAttribute("class", "list-item");
+    li.appendChild(document.createTextNode(taskInput.value + " "));
+    taskList.appendChild(li);
+    taskInput.value = "";
+    let link = document.createElement("a");
+    link.setAttribute("href", "#");
+    link.innerHTML = "x";
+    li.appendChild(link);
+  }
+  e.preventDefault(); // prevent page from being auto refresh on event 'e'
+}
+function removeTask(e) {
+  if (e.target.hasAttribute("href")) {
+    confirm("Are you sure? ");
+    let ele = e.target.parentElement;
+    ele.remove();
+  }
+}
+function removeAllTask(e) {
+  taskList.innerHTML = "";
+}
+function filterTask(e) {
+  let text = filter.value.toLowerCase();
+  document.querySelectorAll(".list-item").forEach((task) => {
+    if (task.textContent.indexOf(text) != -1) {
+      task.style.display = "block";
+    } else task.style.display = "none";
+  });
+  console.log(text);
+  e.preventDefault;
+}
